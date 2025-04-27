ee10a60c19d1a38afc37e2dc3ee53b2d01463259
ryansondergeld <RyanSondergeld@gmail.com>
2025-04-27 17:25:06 -0400

    2P Works

--- ConnectFour_Group3/Game1P.cs
+++ ConnectFour_Group3/Game1P.cs
@@ -1,5 +1,15 @@
-﻿using System.Diagnostics;
-
+﻿//   ___                _ ___ 
+//  / __|__ _ _ __  ___/ | _ \
+// | (_ / _` | '  \/ -_) |  _/
+//  \___\__,_|_|_|_\___|_|_|  
+//                            
+//-----------------------------------------------------------------------------
+// Group 3 
+// CIS 153
+// Sean Kearney
+// Ryan Sondergeld
+// David Sparks
+//-----------------------------------------------------------------------------
 namespace ConnectFour_Group3;
 
 public partial class Game1P : Form
@@ -7,6 +17,7 @@ public partial class Game1P : Form
     private Gfx _gfx;
     private Board _board;
     private int _turn;
+    private bool _playing;
     
     public Game1P()
     {
@@ -16,25 +27,21 @@ public partial class Game1P : Form
         _gfx = new Gfx(this);
         _board = new Board();
         _turn = 1;
+        _playing = true;
         
         // Create Events
         CreateMouseEvents();
-        
-        // Call this to update the graphics
-        _gfx.Update(_board);
-
     }
+    //-------------------------------------------------------------------------
+    // Functions
     //-------------------------------------------------------------------------
     private void Reset()
     {
-        // Generate a new board
+        // Generate a new board, update the graphics, and set the turn
         _board = new Board();
-
-        // Upatte the graphics
         _gfx.Update(_board);
-        
-        // Set turn back to 1st player
         _turn = 1;
+        _playing = true;
     }
     //-------------------------------------------------------------------------
     private void ChangeTurn()
@@ -42,6 +49,38 @@ public partial class Game1P : Form
         if (_turn == 1) { _turn = 2; }
         else { _turn = 1; }
     }
+    //-------------------------------------------------------------------------
+    private void CreateGameOverScreen()
+    {
+        // Create game over pop-up
+        GameOver g = new GameOver();
+            
+        // Set the text
+        if(_turn == 1) { g.SetText("Player 1 Wins!");}
+        else { g.SetText("Player 2 Wins!");}
+        
+        // Set the location
+        g.StartPosition = FormStartPosition.Manual;
+        g.Location = Location;
+
+        g.ShowDialog();
+        
+        // Get the action
+        if (g.GetAction() == "play") { Reset(); }
+        else { Close(); }
+    }
+    //-------------------------------------------------------------------------
+    private void CreateMouseEvents()
+    {
+        for (var i = 0; i < 42; i++)
+        {
+            PictureBox p = _gfx.GetCell(i);
+            p.MouseClick += ColumnClick;
+            p.MouseHover += ColumnHover;
+        }
+    }
+    //-------------------------------------------------------------------------
+    // Event Handlers
     //-------------------------------------------------------------------------
     private void BackButton_Click(object sender, EventArgs e)
     {
@@ -58,23 +97,16 @@ public partial class Game1P : Form
         Reset();
     }
     //-------------------------------------------------------------------------
-    private void CreateMouseEvents()
-    {
-        for (var i = 0; i < 42; i++)
-        {
-            PictureBox p = _gfx.GetCell(i);
-            p.MouseClick += ColumnClick;
-            p.MouseHover += ColumnHover;
-        }
-    }
-    //-------------------------------------------------------------------------
     public void ColumnClick(object sender, EventArgs e)
     {
+        // Guard Clause - Check if we are playing
+        if (!_playing) { return; }
+        
         // Get the PictureBox
         var p = sender as PictureBox;
         
         // Guard Clause = exit if null
-        if (p == null) { return;}
+        if (p == null) { return; }
         
         // Get the column location
         var c = (p.Location.X - 16) / 48;
@@ -83,7 +115,7 @@ public partial class Game1P : Form
         var r = _board.GetRowAvailable(c);
 
         // Guard clause = if the column is full, do nothing on click or hover
-        if (r == -1) { return;}
+        if (r == -1) { return; }
         
         // Otherwise, let's put something in that spot
         _board.SetCell(c, r, _turn);
@@ -92,11 +124,18 @@ public partial class Game1P : Form
         _gfx.Update(_board);
         
         // Check if our move caused a win
-        bool win = _board.CheckWin(c, r);
-        Console.WriteLine(win);
-        
-        // If there is no win, change the turn
-        ChangeTurn();
+        if (_board.CheckWin(c, r))
+        {
+            // Game over Condition here
+            _playing = false;
+
+            CreateGameOverScreen();
+            
+        }
+        else
+        {
+            ChangeTurn();
+        }
 
     }
     //-------------------------------------------------------------------------
--- ConnectFour_Group3/obj/Debug/net8.0-windows/ConnectFour_Group3.AssemblyInfo.cs
+++ ConnectFour_Group3/obj/Debug/net8.0-windows/ConnectFour_Group3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectFour_Group3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9280e3d22e568d513241549d72e541aec80f1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6d24cbfef89778bb5e9981d9ec401bcec8a2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectFour_Group3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectFour_Group3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
